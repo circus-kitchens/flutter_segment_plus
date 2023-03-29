41a87eea0f215406fb0550d905cd720372d9c44a
Marcin Wróblewski <marcin.wroblewski@monterail.com>
2023-03-29 07:47:47 +0200

    New JsonElement conversion

--- android/src/main/kotlin/com/circuskitchens/flutter_segment_plus/FlutterSegmentPlusPlugin.kt
+++ android/src/main/kotlin/com/circuskitchens/flutter_segment_plus/FlutterSegmentPlusPlugin.kt
@@ -228,49 +228,38 @@ fun deepMerge(source: JsonObject, target: JsonObject): JsonObject {
   return merged
 }
 
-fun Any?.toJsonElement(): JsonElement =
-  when (this) {
-    null -> JsonNull
-    is Map<*, *> -> toJsonElement()
-    is Collection<*> -> toJsonElement()
-    is Boolean -> JsonPrimitive(this)
+fun Any?.toJsonElement(): JsonElement {
+  return when (this) {
     is Number -> JsonPrimitive(this)
+    is Boolean -> JsonPrimitive(this)
     is String -> JsonPrimitive(this)
-    is Enum<*> -> JsonPrimitive(this.toString())
-    else -> throw IllegalStateException("Can't serialize unknown type: $this")
+    is Array<*> -> this.toJsonArray()
+    is List<*> -> this.toJsonArray()
+    is Map<*, *> -> this.toJsonObject()
+    is JsonElement -> this
+    else -> JsonNull
   }
-
-private fun Collection<*>.toJsonElement(): JsonElement {
-  val list: MutableList<JsonElement> = mutableListOf()
-  this.forEach { value ->
-    when (value) {
-      null -> list.add(JsonNull)
-      is Map<*, *> -> list.add(value.toJsonElement())
-      is Collection<*> -> list.add(value.toJsonElement())
-      is Boolean -> list.add(JsonPrimitive(value))
-      is Number -> list.add(JsonPrimitive(value))
-      is String -> list.add(JsonPrimitive(value))
-      is Enum<*> -> list.add(JsonPrimitive(value.toString()))
-      else -> throw IllegalStateException("Can't serialize unknown collection type: $value")
-    }
-  }
-  return JsonArray(list)
 }
 
-private fun Map<*, *>.toJsonElement(): JsonElement {
-  val map: MutableMap<String, JsonElement> = mutableMapOf()
-  this.forEach { (key, value) ->
-    key as String
-    when (value) {
-      null -> map[key] = JsonNull
-      is Map<*, *> -> map[key] = value.toJsonElement()
-      is Collection<*> -> map[key] = value.toJsonElement()
-      is Boolean -> map[key] = JsonPrimitive(value)
-      is Number -> map[key] = JsonPrimitive(value)
-      is String -> map[key] = JsonPrimitive(value)
-      is Enum<*> -> map[key] = JsonPrimitive(value.toString())
-      else -> throw IllegalStateException("Can't serialize unknown type: $value")
+fun Array<*>.toJsonArray(): JsonArray {
+  val array = mutableListOf<JsonElement>()
+  this.forEach { array.add(it.toJsonElement()) }
+  return JsonArray(array)
+}
+
+fun List<*>.toJsonArray(): JsonArray {
+  val array = mutableListOf<JsonElement>()
+  this.forEach { array.add(it.toJsonElement()) }
+  return JsonArray(array)
+}
+
+fun Map<*, *>.toJsonObject(): JsonObject {
+  val map = mutableMapOf<String, JsonElement>()
+  this.forEach {
+    if (it.key is String) {
+      map[it.key as String] = it.value.toJsonElement()
     }
   }
   return JsonObject(map)
 }
+
